c0f8e330e5790f418f58b736807d244fe9c86e80
Kush9al <100399261+Kush9al@users.noreply.github.com>
2023-07-31 15:43:24 +0545

    Add files via upload

---
+++ CW-ecommerce/Cw-ecommerce/includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;KushalCommerce | All Rights Reserved | Contact Us: +977 9823150729</b></span></div>
+</footer>
+    
